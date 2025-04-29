694eb97deb77812959756b92f609d24cc98f2a61
Laszlo Lukacs <lukacslacko@gmail.com>
2025-04-29 18:04:37 +0200

    snow

--- src/ui.rs
+++ src/ui.rs
@@ -28,7 +28,7 @@ fn make_globe(n: u32) -> Mesh {
     let m = n + 1;
 
     let perlin = Perlin {
-        seed: 0,
+        seed: 1,
         frequency: 1.0,
         lacunarity: 2.13,
         persistence: 0.5,
@@ -59,39 +59,34 @@ fn make_globe(n: u32) -> Mesh {
                     let (nx, ny, nz) = sphere(u, v);
                     let nr = 5.0;
                     // let color = [u, v, (1 + face) as f32 / 8.0, 1.0];
-                    let noise = perlin.noise(nx, ny, nz) * 5.0;
-                    (noise, [nx * (nr + noise), ny * (nr + noise), nz * (nr + noise)])
+                    let noise = perlin.noise(nx, ny, nz) * 3.0;
+                    (
+                        nr + noise,
+                        [nx * (nr + noise), ny * (nr + noise), nz * (nr + noise)],
+                    )
                 };
                 let normvec = |u, v| {
                     let (_, p) = surface(u, v);
-                    let (_, q) = surface(u + 0.01, v);
-                    let (_, r) = surface(u, v + 0.01);
-                    let a = [
-                        p[0] - q[0],
-                        p[1] - q[1],
-                        p[2] - q[2],
-                    ];
-                    let b = [
-                        p[0] - r[0],
-                        p[1] - r[1],
-                        p[2] - r[2],
-                    ];
+                    let (_, q) = surface(u + 0.001, v);
+                    let (_, r) = surface(u, v + 0.001);
+                    let a = [p[0] - q[0], p[1] - q[1], p[2] - q[2]];
+                    let b = [p[0] - r[0], p[1] - r[1], p[2] - r[2]];
                     let n = [
                         a[1] * b[2] - a[2] * b[1],
                         a[2] * b[0] - a[0] * b[2],
                         a[0] * b[1] - a[1] * b[0],
                     ];
                     let r = (n[0] * n[0] + n[1] * n[1] + n[2] * n[2]).sqrt();
-                    [
-                        n[0] / r,
-                        n[1] / r,
-                        n[2] / r,
-                    ]
+                    [n[0] / r, n[1] / r, n[2] / r]
                 };
                 let (noise, pos) = surface(u, v);
+                /*
                 use std::f32::consts::PI;
                 use std::ops::Add;
-                trait Scalable where Self: Add<f32, Output = f32> + Copy {
+                trait Scalable
+                where
+                    Self: Add<f32, Output = f32> + Copy,
+                {
                     fn scale(&self) -> f32 {
                         (*self + 1.0) / 2.0
                     }
@@ -103,9 +98,39 @@ fn make_globe(n: u32) -> Mesh {
                     (noise + 4.0 * PI / 3.0).sin().scale(),
                     1.0,
                 ];
-                positions.push(pos);
-                colors.push(color);
-                normals.push(normvec(u, v));
+                */
+                let sea_level = 5.0;
+                let snow = 0.5;
+                let height = noise - sea_level;
+                let color = if height > snow {
+                    [1.0, 1.0, 1.0, 1.0]
+                } else {
+                    let v = height / snow;
+                    [v / 2.5, (1.5 - v) / 2.5, v / 5.0, 1.0]
+                };
+                if height > 0.0 {
+                    positions.push(pos);
+                    colors.push(color);
+                    normals.push(normvec(u, v));
+                } else {
+                    let normpos = [pos[0] / noise, pos[1] / noise, pos[2] / noise];
+                    positions.push([
+                        normpos[0] * sea_level,
+                        normpos[1] * sea_level,
+                        normpos[2] * sea_level,
+                    ]);
+                    let blueify = |c: [f32; 4], depth: f32| {
+                        let depth_ratio = (depth / 0.1).clamp(0.0, 1.0).sqrt();
+                        [
+                            c[0] * (1.0 - depth_ratio),
+                            c[1] * (1.0 - depth_ratio),
+                            c[2] * (1.0 - depth_ratio) + depth_ratio,
+                            c[3],
+                        ]
+                    };
+                    colors.push(blueify(color, sea_level - noise));
+                    normals.push(normpos);
+                }
             }
         }
         for i in 0..n {
