bfd9ca2adc115c1cc727763aabf2eef4dbb05650
Julia Pap <papjuli@gmail.com>
2025-05-31 13:26:53 +0200

    add GridPoint type alias

--- src/dijkstra.rs
+++ src/dijkstra.rs
@@ -1,15 +1,13 @@
 use std::collections::HashMap;
 
+type GridPoint = (u32, u32, u32);
+
 #[derive(Default)]
 pub struct GlobePoints {
     pub size: u32,
-    pub points: HashMap<(u32, u32, u32), [f32; 3]>,
+    pub points: HashMap<GridPoint, [f32; 3]>,
 }
 
-pub fn dijkstra(
-    start: (u32, u32, u32),
-    end: (u32, u32, u32),
-    globe_points: &GlobePoints,
-) -> Vec<(u32, u32, u32)> {
+pub fn dijkstra(start: GridPoint, end: GridPoint, globe_points: &GlobePoints) -> Vec<GridPoint> {
     return vec![];
 }
